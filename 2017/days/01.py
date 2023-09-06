5e637c9294b583663bce70f463967f68b5aa29ec
ycatsh <yash.goyal4477@gmail.com>
2023-09-06 18:25:04 +0530

    feat: started 2017

---
+++ 2017/days/01.py
@@ -0,0 +1,24 @@
+with open('../data/01.txt') as file:
+    input = file.read().strip()
+
+
+def part1():
+    digits = []
+    for i in range(len(input)):
+        if input[i] == input[(i+1)-len(input) if i+1 >= len(input) else i+1]:
+            digits.append(int(input[i]))    
+    
+    print(sum(digits))
+
+
+def part2():
+    digits = []
+    for i in range(len(input)):
+        if input[i] == input[int((i+(len(input)/2))-len(input) if i+(len(input)/2) >= len(input) else i+(len(input)/2))]:
+            digits.append(int(input[i]))
+
+    print(sum(digits))
+
+part1()
+part2()
+
---
+++ 2017/days/04.py
@@ -0,0 +1,35 @@
+with open('../data/04.txt') as file:
+    input = file.read().strip().split('\n')
+
+
+def part1():
+    valid = 0
+    for line in input:
+        line = line.split(' ')
+        if len(set(line)) == len(line):
+            valid += 1
+
+    print(valid)
+
+
+def part2():
+    valid = 0 
+    for line in input:
+        line = line.split(' ')
+        line = [sorted(element) for element in line]
+        sorted_pwd = ''
+        sorted_line = []
+        for chars in line:
+            for char in chars:
+                sorted_pwd += char
+            sorted_line.append(sorted_pwd)
+            sorted_pwd = ''
+
+        if len(set(sorted_line)) == len(sorted_line):
+            valid += 1
+
+    print(valid)
+
+
+part1()
+part2()
